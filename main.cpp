e774c6bbd33977b19a86691f540198dbded713fd
xaetris319 <140073478+xaetris319@users.noreply.github.com>
2023-07-24 17:42:55 +0300

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,51 +1,90 @@
 #include <array>
+#include <codecvt>
+#include <deque>
 #include <filesystem>
 #include <fstream>
 #include <iostream>
 #include <string>
 #include <vector>
 
+/*
+ * Зависимости от zxing
+ */
+#include "zxing-cpp-master/core/src/ReadBarcode.h"
+#include "zxing-cpp-master/core/src/GTIN.h"
+#include "zxing-cpp-master/core/ZXVersion.h.in"
+
+#include <cctype>
+#include <chrono>
+#include <cstring>
+// #include <iostream>
+#include <iterator>
+#include <memory>
+// #include <string>
+// #include <vector>
+
+#define STB_IMAGE_IMPLEMENTATION
+#include "stb-master/stb_image.h"
+#define STB_IMAGE_WRITE_IMPLEMENTATION
+#include "stb-master/stb_image_write.h"
+/*
+ * Зависимости от zxing
+ */
+
 #include "messages.hpp"
-#include "file_operations.hpp"
+#include "os_operations.hpp"
 #include "naps2.hpp"
-#include "zxing.hpp"
+#include "zxing_decoder.hpp"
 
 class _main_
 {
 public:
     static void entry()
     {
+        // получаем рабочую директорию в AppData юзера
         set_work_dir();
+        // получаем содержимое входящего файла с данными для программы
         std::string input_content = get_input_content();
+        // 1 - запуск сканера, 2 - простое декодирование файлов в папке
         int mode = get_mode_from_input(input_content);
+        // вектор путей к конкретным изображениям
         std::vector<std::string> images_list;
-        std::string profile_name, file_extension, console_output;
+        // параметры для сканирования, + путь к папке с изображениями
+        std::string profile_name, file_extension, saveto_dir, console_output;
         switch (mode)
         {
         case 1:
+            // случай сканирования, получаем название профиля NAPS2, расширение файлов
             get_profile_info_from_input(input_content, profile_name, file_extension);
-            images_list = naps2::perform_scanning(work_dir, profile_name, file_extension, console_output);
+            // на выходе имеется вектор изображений, папка сохранения, вывод консольки
+            images_list = naps2::perform_scanning(work_dir, profile_name, file_extension, saveto_dir, console_output);
             if (!console_output.empty())
             {
+                // случай, когда NAPS2 сработал с ошибкой
                 make_output_file(message_type::ERROR, console_output, false);
             }
             break;
         case 2:
-            images_list = get_images_list_from_input(input_content);
+            // случай загрузки изображений из папки, получаем вектор и папку сохранения
+            images_list = get_images_list_from_input(input_content, saveto_dir);
             break;
         default:
             break;
         }
         if (images_list.empty())
         {
+            // если вектор изображений пустой - что-то не так
             make_output_file(message_type::ERROR, message::NO_FILES, false);
         }
-        for (std::string const &image : images_list)
-        {
-            std::string barcodes_entry;
-            zxing::decode_image(image, barcodes_entry);
-            barcodes_list.push_back(barcodes_entry);
-        }
+
+        barcodes_list = zxing_decoder::decode_images(saveto_dir, images_list);
+
+        // for (std::string const &image : images_list)
+        // {
+        //     std::string barcodes_entry;
+        //     zxing::decode_image(work_dir, image, barcodes_entry);
+        //     barcodes_list.push_back(barcodes_entry);
+        // }
         make_output_file(message_type::NORMAL, message::EMPTY, true);
     }
 
@@ -73,7 +112,7 @@ private:
     {
         std::string input_location = work_dir + "input.txt";
         std::string input_content;
-        if (file_operations::read_file(input_location, input_content) != 0)
+        if (os_operations::read_file(input_location, input_content) != 0)
         {
             make_output_file(message_type::ERROR, message::NO_INPUT, false);
         }
@@ -110,23 +149,24 @@ private:
     static void get_profile_info_from_input(const std::string &input_content,
                                             std::string &profile_name, std::string &file_extension)
     {
-        size_t pos_start = 0, pos_end, content_len = input_content.length(), sep_len = sep.length();
-        for (int i = 1; pos_end = input_content.find(sep, pos_start) != std::string::npos; i++)
+        size_t pos_start = 0, pos_end, sep_len = sep.length();
+        int counter = 0;
+        while ((pos_end = input_content.find(sep, pos_start)) != std::string::npos)
         {
-            if (i < 2)
+            if (counter > 2)
             {
-                pos_start = pos_end + sep_len;
-            }
-            else if (i == 2)
-            {
-                profile_name = input_content.substr(pos_start, pos_end - pos_start);
-                pos_start = pos_end + sep_len;
-            }
-            else if (i == 3)
-            {
-                file_extension = input_content.substr(pos_start, pos_end - pos_start);
                 break;
             }
+            else if (counter == 1)
+            {
+                profile_name = input_content.substr(pos_start, pos_end - pos_start);
+            }
+            else if (counter == 2)
+            {
+                file_extension = input_content.substr(pos_start, pos_end - pos_start);
+            }
+            pos_start = pos_end + sep_len;
+            counter++;
         }
         if (profile_name.empty() || file_extension.empty())
         {
@@ -134,17 +174,22 @@ private:
         }
     }
 
-    static std::vector<std::string> get_images_list_from_input(const std::string &input_content)
+    static std::vector<std::string> get_images_list_from_input(const std::string &input_content,
+                                                               std::string &saveto_dir)
     {
         std::vector<std::string> images_list;
-        size_t pos_start = 0, pos_end, content_len = input_content.length(), sep_len = sep.length();
+        size_t pos_start = 0, pos_end, sep_len = sep.length();
         std::string token;
-        bool first_segment = true;
+        int segment = 0;
         while ((pos_end = input_content.find(sep, pos_start)) != std::string::npos)
         {
-            if (first_segment)
+            if (segment < 2)
             {
-                first_segment = false;
+                segment++;
+                if (segment == 2)
+                {
+                    saveto_dir = input_content.substr(pos_start, pos_end - pos_start);
+                }
                 pos_start = pos_end + sep_len;
                 continue;
             }
@@ -152,30 +197,29 @@ private:
             pos_start = pos_end + sep_len;
             images_list.push_back(token);
         }
-        token = input_content.substr(pos_start, content_len - pos_start);
-        images_list.push_back(token);
         return images_list;
     }
 
     static void make_output_file(const int &message_code, const std::string &message, const bool &successful)
     {
-        std::string sep = ";";
-        std::string output_content = std::to_string(message_code) + sep + message;
+        std::string _sep = {sep.begin(), sep.end()};
+        std::string output_content = std::to_string(message_code) + _sep + message + _sep;
         if (successful && !barcodes_list.empty())
         {
             for (std::string const &barcode_entry : barcodes_list)
             {
-                output_content = output_content + sep + barcode_entry;
+                output_content = output_content + barcode_entry + _sep;
             }
         }
         std::string output_location = work_dir + "output.txt";
-        file_operations::write_file(output_location, output_content);
+        os_operations::write_file(output_location, output_content);
         exit(0);
     }
 };
 
 int main()
 {
+    setlocale(LC_ALL, "en-US");
     _main_::entry();
     return 0;
 }
--- naps2.hpp
+++ naps2.hpp
@@ -3,10 +3,12 @@ class naps2
 {
 public:
     static std::vector<std::string> perform_scanning(const std::string &work_dir,
-                                                     const std::string &profile_name, const std::string &file_extension,
+                                                     const std::string &profile_name,
+                                                     const std::string &file_extension,
+                                                     std::string &saveto_dir,
                                                      std::string &console_output)
     {
-        std::string saveto_dir = work_dir + get_timestamp() + "\\";
+        saveto_dir = work_dir + os_operations::get_timestamp() + "\\";
         std::string naps2_location = "\"C:\\Program Files (x86)\\NAPS2\\naps2.console\"";
         std::string profile_option = " -p " + profile_name;
         std::string output_option = " -o " + saveto_dir + "DOC$(nnn)." + file_extension;
@@ -14,72 +16,34 @@ public:
         std::string naps2_command = naps2_location + profile_option + output_option + other_options;
         try
         {
-            console_output = exec(naps2_command.c_str());
+            console_output = os_operations::exec(naps2_command.c_str());
         }
         catch (const std::runtime_error &e)
         {
             console_output = e.what();
         }
-        return make_images_list(saveto_dir);
+        return make_images_list(saveto_dir, console_output);
     }
 
 private:
     naps2() {}
     ~naps2() {}
 
-    static std::string add_leading_zeros(const std::string &input, const int &req_length)
-    {
-        std::string result = input;
-        size_t length = input.length();
-        for (size_t i = length; i < req_length; i++)
-        {
-            result = "0" + result;
-        }
-        return result;
-    }
-
-    static std::string tm_to_string(const std::tm &time)
-    {
-        std::string year = std::to_string(time.tm_year + 1900);
-        std::string month = add_leading_zeros(std::to_string(time.tm_mon + 1), 2);
-        std::string day = add_leading_zeros(std::to_string(time.tm_mday), 2);
-        std::string hour = add_leading_zeros(std::to_string(time.tm_hour), 2);
-        std::string minute = add_leading_zeros(std::to_string(time.tm_min), 2);
-        std::string second = add_leading_zeros(std::to_string(time.tm_sec), 2);
-        return year + month + day + "_" + hour + minute + second;
-    }
-
-    static std::string get_timestamp()
-    {
-        struct tm *time_struct;
-        time_t raw_time;
-        time(&raw_time);
-        time_struct = localtime(&raw_time);
-        return tm_to_string(*time_struct);
-    }
-
-    static std::string exec(const char *cmd)
-    {
-        std::array<char, 128> buffer;
-        std::string result;
-        std::unique_ptr<FILE, decltype(&pclose)> pipe(popen(cmd, "r"), pclose);
-        if (!pipe)
-        {
-            throw std::runtime_error("_popen() failed!");
-        }
-        while (fgets(buffer.data(), buffer.size(), pipe.get()) != nullptr)
-        {
-            result += buffer.data();
-        }
-        return result;
-    }
-
-    static std::vector<std::string> make_images_list(std::string saveto_dir)
+    static std::vector<std::string> make_images_list(const std::string &saveto_dir, const std::string &command_output)
     {
         std::vector<std::string> images_list;
-        for (const auto &entry : std::filesystem::directory_iterator(saveto_dir))
+        if (command_output.empty())
         {
-            images_list.push_back(entry.path().string());
+            try
+            {
+                for (const auto &entry : std::filesystem::directory_iterator(saveto_dir))
+                {
+                    images_list.push_back(entry.path().string());
+                }
+            }
+            catch (const std::filesystem::__cxx11::filesystem_error &e)
+            {
+            }
         }
         return images_list;
     }
