6119b792969da55aad0c55be9e375e166dda042c
xaetris319 <140073478+xaetris319@users.noreply.github.com>
2023-08-17 16:08:15 +0300

    Delete old/scan_archive-main directory

--- old/scan_archive-main/messages.hpp
+++
@@ -1,7 +0,0 @@
-#pragma once
-
-namespace messages {
-	const std::string ERROR_NO_PROFILE("Error: couldn\'t read profile.txt. Check ABAP program.");
-	const std::string ERROR_PROFILE_WRONG_CONTENT("Error: profile.txt has wrong content. Check ABAP program.");
-	const std::string ERROR_NO_FILES("Error: couldn\'t find any files after NAPS2 performed the scanning.");
-}
